c013ef559b64c11be73bde54ae9c8db9c13d4a5c
kampoengboy <mike_visualsoft@yahoo.com>
2014-06-01 18:03:53 +0700

    Add Othello Project

---
+++ Tes_othello/Othello.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tes_othello
+{
+    class Othello
+    {
+        public int y = Console.WindowHeight / 2 -5;
+        public int x = Console.WindowWidth / 2 - 20;
+       public void Arena()
+        {
+            for (int k = 0; k < 8; k++)
+            {
+                y = Console.WindowHeight / 2 - 5;
+                for (int i = 0; i <= 8; i++)
+                {
+                    Console.SetCursorPosition(x, y);
+                    Console.WriteLine("*****");
+                    if (i != 8)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            y++;
+                            Console.SetCursorPosition(x, y);
+                            Console.WriteLine("*   *");
+                        }
+
+                    }
+                    y++;
+                }
+                x += 4;
+            }     
+           
+        }
+       public void tampilAwal()
+       {
+           Console.SetCursorPosition(Console.WindowWidth / 2 - 6, Console.WindowHeight / 2 + 9);
+           Console.Write("x");
+           Console.SetCursorPosition(Console.WindowWidth / 2 - 2, Console.WindowHeight / 2 + 9);
+           Console.Write("o");
+           Console.SetCursorPosition(Console.WindowWidth / 2 - 6, Console.WindowHeight / 2 + 13);
+           Console.Write("o");
+           Console.SetCursorPosition(Console.WindowWidth / 2 - 2, Console.WindowHeight / 2 + 13);
+           Console.Write("x");
+       }
+    }
+}
---
+++ Tes_othello/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tes_othello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Othello othello = new Othello();
+            othello.Arena();
+            othello.tampilAwal();
+
+            // index mengeset x & o
+            Console.SetCursorPosition(Console.WindowWidth / 2 - 6, Console.WindowHeight / 2 + 9);
+            Console.ReadKey();
+        }
+    }
+}
